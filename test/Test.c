38b52b6f01dc27ef64df2eaa450a65cc1fcb43a0
Jan Poul <xpoulj01@stud.fit.vutbr.cz>
2016-11-08 13:58:57 +0000

    missing files

---
+++ test/Test.c
@@ -0,0 +1,23 @@
+// #include "unity.h"
+// #include "LED.h"
+// #include <avr/io.h>
+
+// void setUp(void)
+// {
+// }
+
+// void tearDown(void)
+// {
+// }
+
+// void test_LED_Init_should_initialize_the_LED_OFF(void)
+// {
+//     PORTB = 0x55;
+//     DDRB = 0;
+
+//     LED_Init();
+
+//     TEST_ASSERT_BIT_HIGH(DDB5, DDRB);
+
+//     TEST_ASSERT_EQUAL_HEX8(0, PORTB);
+// }
