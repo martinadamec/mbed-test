c02800e57dd351cb2cebd8a6f0c5ccbca08de46a
Martin Adamec <martin.adamec@student.kuleuven.be>
2016-11-21 11:55:12 +0100

    DFE test for blinky

--- Test/blinky.py
+++ Test/blinky.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 
-import sys, os
-sys.path.extend([os.path.join('..', 'DFE_Injector')])
-from main.main import deterDFE
+import time, sys, inspect, os
 
-deterDFE("Test", 1)
+path = os.path.dirname(inspect.getfile(inspect.currentframe()))
+sys.path.insert(1, os.path.join(path, '..', 'Tools'))
+sys.path.extend([os.path.join(path, '..', 'DFE_Injector')])
+
+from main.main import deterDFE
+from CopyToMbed import CopyToMbed
+
+
+
+# Copy file to target adn restart
+inst = CopyToMbed(sys.argv[1:])
+testFilename = inst.copy(True)
+time.sleep(1)
+
+try:
+	deterDFE("Test", 1)
+except Exception as e:
+	raise # re-raise the error
+finally:
+	try:
+	    os.remove(testFilename)
+	    inst.restartMbed()
+	except OSError:
+		pass
